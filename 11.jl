42fd359e3d12f45737a20fde17a3acd120ca9a95
Ludovic Stephan <ludovic.stephan@protonmail.com>
2023-12-18 18:11:40 +0100

    Day 11 but cleaner

--- 11.jl
+++ 11.jl
@@ -1,21 +1,16 @@
 line_to_vec(line) = collect(line) .== '#'
-offset((x, y), x_off, y_off; multiplier=1) = x + multiplier * x_off[x], y + multiplier * y_off[y]
-manhattan(x, y) = sum(abs, x .- y)
-sum_dist(galaxies) = sum(manhattan(x, y) for x in galaxies for y in galaxies) ÷ 2
-
-function sum_expand(base_galaxies, row_offsets, col_offsets, multiplier)
-    expanded = offset.(base_galaxies, Ref(col_offsets), Ref(row_offsets), multiplier=multiplier)
-    return sum_dist(expanded)
-end
+sum_dist(v) = sum(abs(x-y) for x in v for y in v) ÷ 2
+expand(x, offset, multiplier) = x + multiplier * offset[x]
+sum_expand(x, offset, multiplier) = expand(x, offset, multiplier) |> sum_dist 
 
 open(ARGS[1]) do file
     universe = hcat(line_to_vec.(eachline(file))...)
-    m, n = size(universe)
-    row_offsets = (1:m) - cumsum(vec(any(universe, dims=1)))
-    col_offsets = (1:n) - cumsum(vec(any(universe, dims=2)))
+    empty_cols = cumsum(vec(.!any(universe, dims=1)))
+    empty_rows = cumsum(vec(.!any(universe, dims=2)))
     base_galaxies = Tuple.(findall(universe))
-    part1 = sum_expand(base_galaxies, row_offsets, col_offsets, 1)
-    part2 = sum_expand(base_galaxies, row_offsets, col_offsets, 10^6-1)
+    x, y = first.(base_galaxies), last.(base_galaxies)
+    part1 = sum_expand(x, empty_rows, 2) + sum_expand(y, empty_cols, 2)
+    part2 = sum_expand(x, empty_rows, 10^6) + sum_expand(y, empty_cols, 10^6)
     println("Part 1 : $part1")
     println("Part 2 : $part2")
 end
